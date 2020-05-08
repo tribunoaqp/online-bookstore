c6d49fa0a68b0dbbc3b7d69f4e3f0a449a60bbf8
TribunoAqp <osccoem@gmail.com>
2020-05-07 19:15:07 -0500

    Creación del proyecto Angular y se completó el feature 1.

--- onlinebookstore/src/main/java/in/maoesoft/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/in/maoesoft/onlinebookstore/repository/BookRepository.java
@@ -2,9 +2,11 @@ package in.maoesoft.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 //import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.maoesoft.onlinebookstore.entity.Book;
 
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends  JpaRepository<Book, Long> {
 
 }
