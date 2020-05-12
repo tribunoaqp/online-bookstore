7c78c62099d8a7d882a7b326b915399cdc86b3b5
TribunoAqp <osccoem@gmail.com>
2020-05-11 22:28:08 -0500

    Featured 3 completado.

--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../../common/book';
 import { BookService } from '../../services/book.service';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -12,16 +13,30 @@ import { BookService } from '../../services/book.service';
 export class BookListComponent implements OnInit {
 
   books: Book[];
+  currentCategoryId: number;
 
-
-  constructor(private _bookService: BookService) { }
+  constructor(private _bookService: BookService,
+              private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listBooks();
+    this._activatedRoute.paramMap.subscribe(() => {
+      this.listBooks();
+    })
   }
 
   listBooks(): void {
-    this._bookService.getBooks().subscribe(
+
+    const hasCategoryId: boolean = this._activatedRoute.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+      this.currentCategoryId = +this._activatedRoute.snapshot.paramMap.get('id');
+    }else{
+      this.currentCategoryId = 1;
+    }
+
+
+
+    this._bookService.getBooks(this.currentCategoryId).subscribe(
       data => this.books = data
     )
   }
--- onlinebookstore/src/main/java/in/maoesoft/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/in/maoesoft/onlinebookstore/repository/BookRepository.java
@@ -1,6 +1,10 @@
 package in.maoesoft.onlinebookstore.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 //import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -9,4 +13,6 @@ import in.maoesoft.onlinebookstore.entity.Book;
 @CrossOrigin("http://localhost:4200")
 public interface BookRepository extends  JpaRepository<Book, Long> {
 
+	@RestResource(path = "categoryid")
+	Page<Book> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
